ecba75768ab3655a624363de7fc91bdef8d6e602
JeanMichel FRANCOIS <toutpt@gmail.com>
2011-12-29 17:41:10 +0100

    add googletest

--- README.rst
+++ README.rst
@@ -18,3 +18,7 @@ execute this command:
     bin/buildout
     bin/python gaselenium.py http://monsite.com/sitemap.xml
 
+Google test
+===========
+
+    bin/python googletest.py http://www.inextcom.fr /home/toutpt/tmp/keywords.txt
---
+++ googletest.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib
+import urlparse
+import sys
+
+domain = sys.argv[1]
+keywords = open(sys.argv[2]).read().split('\n')
+
+def check_domain(url):
+    if not url.startswith('http://www.google'):
+        return url.startswith(domain)
+    o = urlparse.urlparse(url)
+    params = o.query.split('&')
+    for param in params:
+        if param.startswith('url='):
+            rdomain = urllib.unquote(param[4:])
+            return domain == rdomain
+    return False
+
+browser = webdriver.Firefox() # Get local session of firefox
+query = {'q':None,'hl':'fr'}
+for keyword in keywords:
+    query['q'] = keyword
+    url = 'http://www.google.fr/?%s'%urllib.urlencode(query)
+    browser.get(url)
+    browser.find_element_by_name('btnK').click()
+    time.sleep(1)
+    results = browser.find_elements_by_class_name('l')
+    for result in results:
+        if check_domain(result.get_attribute('href')):
+            result.click()
+            time.sleep(2)
+            break
+    time.sleep(2)
+    browser.delete_all_cookies()
+browser.close()
